fb859734fb6fe236e0dfe25a5e7b61ec496554d8
franc <franc@DESKTOP-4PUV6IP.mshome.net>
2025-05-14 21:20:24 +0200

    Primer commit

---
+++ primer_proyecto_git/src/main/java/com/mycompany/primer_proyecto_git/Primer_proyecto_git.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primer_proyecto_git;
+
+/**
+ *
+ * @author franc
+ */
+public class Primer_proyecto_git {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
